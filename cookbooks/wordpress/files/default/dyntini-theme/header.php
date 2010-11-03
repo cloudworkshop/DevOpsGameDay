c4bec3b596622e70dcd191c1b9f99cf2b6fa13fe
Kevin Gray <kgray@kgray-ubuntu.(none)>
2010-11-03 06:19:49 -0700

    update to the header to display the server's ip address

--- cookbooks/wordpress/files/default/dyntini-theme/header.php
+++ cookbooks/wordpress/files/default/dyntini-theme/header.php
@@ -10,7 +10,7 @@
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?> - Served from www-02-west.reserve.dyntini.com</title>
+<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?> - Served by http://reserve.dyntini.com ( <?php echo $_SERVER['SERVER_ADDR']; ?> )</title>
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
