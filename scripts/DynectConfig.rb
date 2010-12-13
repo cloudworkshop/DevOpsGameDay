b18cc00744097aa474276066a110a4ede9410ae0
kgray <kgray@dyn.com>
2010-12-13 11:40:35 -0800

    config file for dynect remove A Record ruby script

---
+++ scripts/DynectConfig.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+module DynectConfig
+   DYN_CN = "<customer_name>" # dynect customer name
+   DYN_UN = "<user_name>" # dynect user name
+   DYN_PWD = "<password>" # dynect password
+   DYN_FQDN = "<fqdn>" #fqdn of A records
+   DYN_ZONE = "<zone>" #zone of A Records
+end
